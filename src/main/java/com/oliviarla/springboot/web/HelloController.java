e3781ff9a0b37c585fc4bf808961bbe858619b14
minkyeong <alsrud713@gmail.com>
2021-01-15 21:22:57 +0900

    add lombok

--- src/main/java/com/oliviarla/springboot/web/HelloController.java
+++ src/main/java/com/oliviarla/springboot/web/HelloController.java
@@ -1,14 +1,21 @@
 package com.oliviarla.springboot.web;
 
+import com.oliviarla.springboot.web.dto.HelloResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("/hello")
     public String hello(){
         return "hello";
     }
 
+    @GetMapping("hello/dto")
+    public HelloResponseDto helloResponseDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
 }
---
+++ src/test/java/com/oliviarla/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,25 @@
+package com.oliviarla.springboot.web.dto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombok_test(){
+        String name = "test";
+        int amount = 1000;
+
+        //when
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+
+       assertThat(dto.getName()).isEqualTo(name);
+       assertThat(dto.getAmount()).isEqualTo(amount);
+
+    }
+
+}

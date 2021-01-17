73598173b9436d6ddbbb76f3c1c9140e120720fe
minkyeong <alsrud713@gmail.com>
2021-01-17 22:26:59 +0900

    add edit function

--- src/main/java/com/oliviarla/springboot/web/HelloController.java
+++ src/main/java/com/oliviarla/springboot/web/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
         return "hello";
     }
 
-    @GetMapping("hello/dto")
+    @GetMapping("/hello/dto")
     public HelloResponseDto helloResponseDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
         return new HelloResponseDto(name, amount);
     }
